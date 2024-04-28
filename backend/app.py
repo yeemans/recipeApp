28c5d79accb9b2bc6544ac731a9650be77d0e6de
yeemans <arthur.roth.07@gmail.com>
2024-04-28 00:01:58 -0700

    started isLoggedIn functionality

--- backend/app.py
+++ backend/app.py
@@ -38,7 +38,7 @@ def get_users():
     users_json = jsonify(users)
     return users_json
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['POST'])
 def register():
     session_token = None
     data = json.loads(request.data)
@@ -47,7 +47,7 @@ def register():
     password = generate_password_hash(password)
 
     cur = conn.cursor()
-    if check_user_exists(username):
+    if check_user_exists(cur, username):
         return jsonify({'message': 'Username is already used.', 'success': False})
     else:
         inserted_user_id = insert_new_user(cur, username, password)
@@ -57,14 +57,56 @@ def register():
     cur.close()
     return jsonify({'session_token': session_token, 'success': True})
 
-def check_user_exists(username):
+@app.route('/login', methods=['POST'])
+def login():
+    data = json.loads(request.data)
+    username = data['username']
+    password = data['password']
     cur = conn.cursor()
+
+    if not check_user_exists(cur, username):
+        return jsonify({'message': 'Wrong username/password', 'success': False})
+    
+    if not check_password(cur, username, password):
+        return jsonify({'message': 'Wrong username/password', 'success': False})
+    
+    session_token = create_session(cur, get_user(cur, username))
+    cur.close()
+    return jsonify({'session_token': session_token, 'success': True})
+
+@app.route("/logged_in", methods=['POST'])
+def logged_in():
+    data = json.loads(request.data)
+    username = data['username']
+    session_token = data['session_token']
+    cur = conn.cursor()
+
+    if not check_user_exists(cur, username):
+        print(f"{username} does not exist.")
+        return jsonify({'success': False})
+    
+    session = get_session(cur, username, session_token)
+    cur.close()
+
+    if not session: 
+        print(f"session not found")
+        return jsonify({'success': False})
+    return jsonify({'success': True})
+
+
+def check_user_exists(cur, username):
     query = sql.SQL("SELECT 1 FROM users WHERE username = {}").format(sql.Literal(username))
     cur.execute(query)
 
     user_exists = cur.fetchone() is not None
-    cur.close()
     return user_exists
+
+def check_password(cur, username, login_password):
+    query = sql.SQL("SELECT password FROM users WHERE username = {}").format(sql.Literal(username))
+    cur.execute(query)
+
+    database_password = cur.fetchone()[0]
+    return check_password_hash(database_password, login_password)
 
 def insert_new_user(cur, username, password):
     sql_insert = """
@@ -76,15 +118,31 @@ def insert_new_user(cur, username, password):
     inserted_user_id = cur.fetchone()[0]
     return inserted_user_id
 
-def create_session(cur, inserted_user_id):
-        sql_insert = """
-        INSERT INTO sessions (user_id, session_token)
-        VALUES (%s, %s);
-        """
+def get_user(cur, username):
+    sql_insert = """
+    SELECT id FROM users 
+    WHERE username = %s
+    """
+    cur.execute(sql_insert, (username,))
+    user_id = cur.fetchone()[0]
+    return user_id
 
-        session_token = secrets.token_hex(16)
-        cur.execute(sql_insert, (inserted_user_id, session_token))
-        return session_token
+def create_session(cur, inserted_user_id):
+    sql_insert = """
+    INSERT INTO sessions (user_id, session_token)
+    VALUES (%s, %s);
+    """
+    session_token = secrets.token_hex(16)
+    cur.execute(sql_insert, (inserted_user_id, session_token))
+    return session_token
+
+def get_session(cur, username, session_token):
+    sql_insert = """
+    SELECT * FROM sessions 
+    WHERE user_id = %s AND session_token = %s
+    """
+    cur.execute(sql_insert, (get_user(cur, username), session_token,))
+    return cur.fetchone() is not None
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,7 +1,28 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function Home() {
     const { username } = useParams();
+    let navigate = useNavigate();
+    useEffect(() => {
+        checkLoggedIn();
+    }, [])
+
+    async function checkLoggedIn() {
+        let sessionToken = sessionStorage.getItem("recipeAppSession");
+        console.log(sessionToken);
+        if (sessionToken === null) return navigate(`/login`);
+
+        let result = await axios.post("http://localhost:5000/logged_in", {
+            username: username,
+            session_token: sessionToken,
+        });
+
+        // bounce user back to login if not logged in
+        if (!result["data"]["success"]) return navigate(`/login`);
+    }
+
     return(
         <div>
             <p>Welcome {username}</p>
